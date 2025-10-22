9b26d5023ce9544700d3b6bd529049f858a41e85
lukaskarljunker-design <lukaskarljunker@gmail.com>
2025-10-22 18:24:11 +0200

    Add files via upload

---
+++ coachingzone.js
@@ -0,0 +1,8 @@
+/* Coaching Zone – Vanilla JS (BVRP Style)
+--------------------------------------------------
+Diese Datei ist vollständig eigenständig und kann
+direkt auf GitHub hochgeladen werden.
+Einbindung siehe Readme oder Chat.
+*/
+
+(function(){console.log('Coaching Zone script loaded');})();
